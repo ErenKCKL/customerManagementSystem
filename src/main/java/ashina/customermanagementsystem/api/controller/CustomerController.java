9858d2ffb360db55862200976572bd56de7a4a24
Eren <eren092003@gmail.com>
2023-07-11 18:36:09 +0200

    new changes

--- src/main/java/ashina/customermanagementsystem/api/controller/CustomerController.java
+++ src/main/java/ashina/customermanagementsystem/api/controller/CustomerController.java
@@ -3,8 +3,11 @@ package ashina.customermanagementsystem.api.controller;
 import ashina.customermanagementsystem.business.abstracts.CustomerService;
 import ashina.customermanagementsystem.entities.concretes.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -24,6 +27,22 @@ public class CustomerController {
         Customer updatedCustomer = customerService.updateCustomer(customer);
         return ResponseEntity.ok(updatedCustomer);
     }
+
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int id){
+        return customerService.findById(id).map(customer -> {
+            customerService.deleteById(id);
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/getAllCustomer")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        List<Customer> customers = customerService.getAllCustomer();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+
 
 
 }
