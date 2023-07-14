c1ae829285e5686dc12c1645e88e970a8ffa1a66
Eren <eren092003@gmail.com>
2023-07-14 17:13:25 +0200

    Session Helper Update

--- src/main/java/ashina/customermanagementsystem/entities/concretes/SessionHelper.java
+++ src/main/java/ashina/customermanagementsystem/entities/concretes/SessionHelper.java
@@ -2,12 +2,21 @@ package ashina.customermanagementsystem.entities.concretes;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Component
 public class SessionHelper {
+
+    private Map<Integer, String> sessionTokens = new HashMap<>();
     public  String generateSessionToken(int id) {
+        if (sessionTokens.containsKey(id)) {
+            return sessionTokens.get(id);
+        }
+
         String sessionToken = UUID.randomUUID().toString();
+        sessionTokens.put(id, sessionToken);
         return sessionToken;
     }
 
